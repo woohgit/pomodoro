ed00351cbe21e71595d980bfd01b62a06d0c1f0f
Adam Papai <wooh@wooh.hu>
2016-09-01 19:03:19 +0000

    [WiP] Reformat code + interrupt handling

--- pomodoro.js
+++ pomodoro.js
@@ -12,9 +12,18 @@ i = workingPomodoro;
 selected_task = '';
 
 
-coffeText = {image:'<i class="fa fa-coffee fa-5x" aria-hidden="true"></i>', text:"It's time for a coffee break!"};
-workText  = {image:'<i class="fa fa-keyboard-o fa-5x" aria-hidden="true"></i>', text:"It's time to go back to work!"};
-longbreakText = {image:'<i class="fa fa-coffee fa-5x" aria-hidden="true"></i>', text:"It's time for a long break!"};
+coffeText = {
+    image: '<i class="fa fa-coffee fa-5x" aria-hidden="true"></i>',
+    text: "It's time for a coffee break!"
+};
+workText = {
+    image: '<i class="fa fa-keyboard-o fa-5x" aria-hidden="true"></i>',
+    text: "It's time to go back to work!"
+};
+longbreakText = {
+    image: '<i class="fa fa-coffee fa-5x" aria-hidden="true"></i>',
+    text: "It's time for a long break!"
+};
 
 coffeeButtonText = "Take a break!"
 workButtonText = "Back to work!"
@@ -32,36 +41,38 @@ internalInterrupt = '</span><span class="glyphicon glyphicon-exclamation-sign" a
 
 
 function loadTodo() {
-  
-  var data = null;
-  
-  try {
-    data = JSON.parse(localStorage.getItem("todoData"));
-  } catch(err) { }
 
-  // an empty array is a valid data source for the the bootstap-table
-  if ( data === null ) {
-    data = [];
-  }
-  
-  return data;
+    var data = null;
+
+    try {
+        data = JSON.parse(localStorage.getItem("todoData"));
+    }
+    catch (err) {}
+
+    // an empty array is a valid data source for the the bootstap-table
+    if (data === null) {
+        data = [];
+    }
+
+    return data;
 }
 
 
 function loadUnplanned() {
 
-  var data = null;
+    var data = null;
 
-  try {
-    data = JSON.parse(localStorage.getItem("unplannedData"));
-  } catch(err) { }
+    try {
+        data = JSON.parse(localStorage.getItem("unplannedData"));
+    }
+    catch (err) {}
 
-  // an empty array is a valid data source for the the bootstap-table
-  if ( data === null ) {
-    data = [];
-  }
+    // an empty array is a valid data source for the the bootstap-table
+    if (data === null) {
+        data = [];
+    }
 
-  return data;
+    return data;
 }
 
 function formatSeconds(seconds) {
@@ -75,43 +86,50 @@ function formatSeconds(seconds) {
 }
 
 function calculatePercent(seconds) {
-    s = seconds / pomodoroConfig[counter-1] * 100;
+    s = seconds / pomodoroConfig[counter - 1] * 100;
     return s;
 }
 
 function startCounting() {
-    
+
     if (counter >= 8) {
         counter = 0;
+        $('#pbar').removeClass('active');
+    } else {
+        $('#pbar').addClass('active');
     }
 
     i = pomodoroConfig[counter];
-    
+
     counter++;
-    
-    counterBack = setInterval(function () {
+
+    counterBack = setInterval(function() {
         i--;
         if (i >= 0) {
             $('.progress-bar').css('width', calculatePercent(i) + '%');
             $('#panel-footer').text(formatSeconds(i));
-        } else {
+        }
+        else {
             notifyMe('Times UP!', messageConfig[counter].text);
             clearInterval(counterBack);
             var modal = $('#myModal')
             modal.find('.modal-title').html(messageConfig[counter].image);
             modal.find('.modal-body').html(messageConfig[counter].text);
+            $('.progress-bar').css('width', '100%');
+            $('#pbar').removeClass('active');
             // Every odd number is a pomodoro
             // don't let switching task before a break!
             // also increase pomodoros only when working not when having a break
-            if ( counter % 2 > 0) {
-              $("#switchTask").hide();
-              increasePomodoro();
-              loadTasks();
-            } else {
-              $("#switchTask").show();
+            if (counter % 2 > 0) {
+                $("#switchTask").hide();
+                increasePomodoro();
+                loadTasks();
+            }
+            else {
+                $("#switchTask").show();
             }
             $('#closeModal').text(closeButtonText[counter]);
-            $('#myModal').modal({   
+            $('#myModal').modal({
                 keyboard: false
             });
         }
@@ -120,17 +138,17 @@ function startCounting() {
 }
 
 // request permission on page load
-document.addEventListener('DOMContentLoaded', function () {
-  if (!Notification) {
-    alert('Desktop notifications not available in your browser. Try Chromium.'); 
-    return;
-  }
+document.addEventListener('DOMContentLoaded', function() {
+    if (!Notification) {
+        alert('Desktop notifications not available in your browser. Try Chromium.');
+        return;
+    }
 
-  if (Notification.permission !== "granted")
-    Notification.requestPermission();
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
 });
 
-function audioNotification(){
+function audioNotification() {
     var alertSound = new Audio('demonstrative.mp3');
     alertSound.play();
 }
@@ -140,160 +158,189 @@ function notifyMe(title, message) {
     try {
         settings = JSON.parse(localStorage.getItem("pomodoro_settings"));
         if (settings.notification == false) {
-          return;
+            return;
         }
-    } catch(err) { 
-      return;
     }
-  
-  
-  if (Notification.permission !== "granted")
-      Notification.requestPermission();
-  else {
-    audioNotification();
-    var notification = new Notification(title, {
-      icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Ic_timer_48px.svg/48px-Ic_timer_48px.svg.png',
-      body: message,
-    });
+    catch (err) {
+        return;
+    }
 
-    notification.onclick = function () {
-      window.open("http://stackoverflow.com/a/13328397/1269037");      
-    };
-  }
+
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
+    else {
+        audioNotification();
+        var notification = new Notification(title, {
+            icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Ic_timer_48px.svg/48px-Ic_timer_48px.svg.png',
+            body: message,
+        });
+
+        notification.onclick = function() {
+            window.open("http://stackoverflow.com/a/13328397/1269037");
+        };
+    }
 }
 
 function loadTasks() {
-    // depending on the selected menu
-    // load the todos or the unplanned and urgent data
-    if ( $('#todo_menu').hasClass('active') ) {
-        data = loadTodo();
-        $('#todo_table').bootstrapTable('load', data);
+    data = loadTodo();
+    $('#todo_count').html(data.length);
+    $('#todo_table').bootstrapTable('load', data);
 
-    } else {
-        data = loadUnplanned();
-        $('#unplanned_table').bootstrapTable('load', data);
-    }
+    data = loadUnplanned();
+    $('#unplanned_count').html(data.length);
+    $('#unplanned_table').bootstrapTable('load', data);
 }
 
 function populateTasks() {
 
-  data = loadTodo();
-  $('#todo_count').html(data.length);
+    data = loadTodo();
+    $('#todo_count').html(data.length);
 
-  $('#todo_table').bootstrapTable({
-      columns: [{
-           checkbox: true,
-           formatter: stateFormatter
-      }, { width: 400,
-           field: 'task',
-           title: 'Task name'
-      }, { field: 'pomodoros',
-           title: pomodoroGlyph + ' Pomodoros'
-      }, {
-           field: 'internal',
-           title: internalInterrupt + ' Internal'
-      },  {
-           field: 'external',
-           title: externalInterrupt + ' External'
-      }],
-      data: data
-  });
+    $('#todo_table').bootstrapTable({
+        columns: [{
+            checkbox: true,
+            formatter: stateFormatter
+        }, {
+            width: 400,
+            field: 'task',
+            title: 'Task name'
+        }, {
+            field: 'pomodoros',
+            title: pomodoroGlyph + ' Pomodoros'
+        }, {
+            field: 'internal',
+            title: internalInterrupt + ' Internal'
+        }, {
+            field: 'external',
+            title: externalInterrupt + ' External'
+        }],
+        data: data
+    });
 
 
-  data = loadUnplanned();
-  $('#unplanned_count').html(data.length);
+    data = loadUnplanned();
+    $('#unplanned_count').html(data.length);
 
-  $('#unplanned_table').bootstrapTable({
-      columns: [{
-           checkbox: true,
-           formatter: stateFormatter
-      }, { width: 400,
-           field: 'task',
-           title: 'Cause'
-      }],
-      data: data
-  });
+    $('#unplanned_table').bootstrapTable({
+        columns: [{
+            checkbox: true,
+            formatter: stateFormatter
+        }, {
+            width: 400,
+            field: 'task',
+            title: 'Unplanned task'
+        }],
+        data: data
+    });
 }
 
 function saveTasks(data) {
-  localStorage.setItem("todoData", JSON.stringify(data));
+    localStorage.setItem("todoData", JSON.stringify(data));
 }
 
 
 function addNewTodoItem(name) {
-  data = loadTodo();
-  data.push({task: name, pomodoros: 0, external: 0, internal: 0, done: false});
-  saveTasks(data);
-  loadTasks();
+    data = loadTodo();
+    data.push({
+        task: name,
+        pomodoros: 0,
+        external: 0,
+        internal: 0,
+        done: false
+    });
+    saveTasks(data);
+    loadTasks();
 }
 
+
+function saveUnplanned(data) {
+    localStorage.setItem("unplannedData", JSON.stringify(data));
+}
+
+
+function addNewUnplanned(name) {
+    data = loadUnplanned();
+    data.push({
+        task: name
+    });
+    saveUnplanned(data);
+    loadTasks();
+}
 
 
 function initializeSettings() {
-  
-  try {
 
-    var lb = JSON.parse(localStorage.getItem("pomodoro_settings"));
-    longBreak = lb;
+    try {
 
-  } catch(err) {
+        var lb = JSON.parse(localStorage.getItem("pomodoro_settings"));
+        longBreak = lb;
 
-    longBreak = 25*60;
+    }
+    catch (err) {
 
-  }
+        longBreak = 25 * 60;
+
+    }
 
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-  initializeSettings();
-  
-  //localStorage.clear();
-  populateTasks();
+    initializeSettings();
+
+    //localStorage.clear();
+    populateTasks();
 });
 
 function cellStyle(value, row, index, field) {
-  return {
-    css: {"font-size": "10px"}
-  };
+    return {
+        css: {
+            "font-size": "10px"
+        }
+    };
 }
 
 
 function stateFormatter(value, row, index) {
-  
+
     st = getSelectedTask();
     row.pomodoros = pomodoroGlyph.repeat(row.pomodoros);
     row.internal = internalInterrupt.repeat(row.internal);
     row.external = externalInterrupt.repeat(row.external);
+
     if (st != null) {
-      if (row.task == st.task) {
-        return {
-            checked: true
-        };
-      }
+        if (row.task == st.task) {
+            return {
+                checked: true
+            };
+        }
     }
     if (row.done == true) {
-      row.task = "<strike>" + row.task + "</strike>";
-      return {
-          checked: false
-      }
+        row.task = "<strike>" + row.task + "</strike>";
+        return {
+            checked: false
+        }
     }
     return value;
 }
 
 
 function convertStringTimeToSeconds(text) {
-  var a = text.split(':');
-  var seconds = (+a[0]) * 60 + (+a[1]);
-  return seconds;
+    var a = text.split(':');
+    var seconds = (+a[0]) * 60 + (+a[1]);
+    return seconds;
 }
 
 function saveSettings(longBreak, notification, shortBreak) {
-  
-  long_break = convertStringTimeToSeconds(longBreak);
-  short_break = convertStringTimeToSeconds(shortBreak);
-  data = {'long_break': long_break, 'notification': notification, 'short_break': short_break};
-  localStorage.setItem("pomodoro_settings", JSON.stringify(data));
+
+    long_break = convertStringTimeToSeconds(longBreak);
+    short_break = convertStringTimeToSeconds(shortBreak);
+    data = {
+        'long_break': long_break,
+        'notification': notification,
+        'short_break': short_break
+    };
+    localStorage.setItem("pomodoro_settings", JSON.stringify(data));
 }
 
 function saveSelectedTask(task) {
@@ -304,22 +351,23 @@ function getSelectedTask() {
     selected_task = null;
     try {
         selected_task = JSON.parse(localStorage.getItem("selected_task"));
-    } catch (e) {}
+    }
+    catch (e) {}
 
     return selected_task;
 }
 
 function increasePomodoro() {
-  
+
     st = getSelectedTask();
     task_name = st.task;
-    
+
     data = loadTodo();
     for (var key in data) {
         if (data[key].task == task_name) {
             data[key].pomodoros += 1;
         }
-            
+
     }
     saveTasks(data);
     loadTasks();
@@ -327,20 +375,20 @@ function increasePomodoro() {
 }
 
 function removeSelected() {
-  
+
     st = $('#todo_table').bootstrapTable('getSelections');
-    
+
     if (st.length == 0) {
-      return;
+        return;
     }
 
-    task_name = st[0].task.replace(/(<([^>]+)>)/ig,"");
+    task_name = st[0].task.replace(/(<([^>]+)>)/ig, "");
 
     data = loadTodo();
     var i = 0;
     for (var key in data) {
         if (data[key].task == task_name) {
-          break;
+            break;
         }
         i++;
     }
@@ -355,17 +403,17 @@ function markDoneSelected() {
     st = $('#todo_table').bootstrapTable('getSelections');
 
     if (st == '') {
-      return;
+        return;
     }
     st = st[0];
 
     task_name = st.task;
-    
+
     data = loadTodo();
     var i = 0;
     for (var key in data) {
         if (data[key].task == task_name) {
-          data[key].done = true;
+            data[key].done = true;
         }
         i++;
     }
@@ -373,50 +421,68 @@ function markDoneSelected() {
 }
 
 function isTaskDisabled(task) {
-    task_name = task.task.replace(/(<([^>]+)>)/ig,"");
+    task_name = task.task.replace(/(<([^>]+)>)/ig, "");
     data = loadTodo();
     var i = 0;
     for (var key in data) {
         if (data[key].task == task_name) {
-          if (data[key].done == true) {
-            return true;
-          } else {
-            return false;
-          }
+            if (data[key].done == true) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
     }
 }
 
 function disableButtonsDuringPomodoro() {
+
     $('#internalButton').removeClass('disabled');
     $('#externalButton').removeClass('disabled');
-    
+    $('#internalButton').prop('disabled', false);
+    $('#externalButton').prop('disabled', false);
+
     $('#addTask').addClass('disabled');
+    $('#addTask').prop('disabled', true);
+
     $('#removeSelected').addClass('disabled');
+    $('#removeSelected').prop('disabled', true);
+
     $('#markAsDone').addClass('disabled');
+    $('#markAsDone').prop('disabled', true);
     $('#rulesModal').addClass('disabled');
+    $('#rulesModal').prop('disabled', true);
     $('#settingsModalButton').addClass('disabled');
+    $('#settingsModalButton').prop('disabled', true);
 }
 
 function enableButtonsDuringNonPomodoro() {
-  $('#internalButton').addClass('disabled');
-  $('#externalButton').addClass('disabled');
-  
-  $('#addTask').removeClass('disabled');
-  $('#removeSelected').removeClass('disabled');
-  $('#markAsDone').removeClass('disabled');
-  $('#rulesModal').removeClass('disabled');
-  $('#settingsModalButton').removeClass('disabled');
+    $('#internalButton').addClass('disabled');
+    $('#internalButton').prop('disabled', true);
+    $('#externalButton').addClass('disabled');
+    $('#externalButton').prop('disabled', true);
+
+    $('#addTask').removeClass('disabled');
+    $('#addTask').prop('disabled', false);
+    $('#removeSelected').removeClass('disabled');
+    $('#removeSelected').prop('disabled', false);
+    $('#markAsDone').removeClass('disabled');
+    $('#markAsDone').prop('disabled', false);
+    $('#rulesModal').removeClass('disabled');
+    $('#rulesModal').prop('disabled', false);
+    $('#settingsModalButton').removeClass('disabled');
+    $('#settingsModalButton').prop('disabled', false);
 }
 
 function getLongBreak() {
 
     settings = JSON.parse(localStorage.getItem("pomodoro_settings"));
-    if ( settings == null ) {
+    if (settings == null) {
         return DEFAULT_LONG_BREAK;
     }
 
-    if ( settings.long_break == null) {
+    if (settings.long_break == null) {
         return DEFAULT_LONG_BREAK
     }
 
@@ -425,13 +491,40 @@ function getLongBreak() {
 
 function getShortBreak() {
     settings = JSON.parse(localStorage.getItem("pomodoro_settings"));
-    if ( settings == null ) {
+    if (settings == null) {
         return DEFAULT_SHORT_BREAK;
     }
 
-    if ( settings.short_break == null) {
+    if (settings.short_break == null) {
         return DEFAULT_SHORT_BREAK
     }
 
     return settings.short_break;
+}
+
+function increaseInterrupt(type) {
+
+    st = getSelectedTask();
+    task_name = st.task;
+
+    data = loadTodo();
+    for (var key in data) {
+        if (data[key].task == task_name) {
+            if (type == 'internal') {
+                data[key].internal += 1;
+            }
+            else {
+                data[key].external += 1;
+            }
+        }
+
+    }
+    saveTasks(data);
+    loadTasks();
+}
+
+function cleanupUnplanned() {
+    data = [];
+    saveUnplanned(data);
+    loadTasks();
 }
